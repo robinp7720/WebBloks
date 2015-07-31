bdceed6a1d3c747ca369f24cc0b43cad191d3549
robinp7720 <robin@decker.cx>
2015-07-31 16:30:19 +0100

    New much more expandable block objects

--- js/blocks.js
+++ js/blocks.js
@@ -25,135 +25,59 @@ blocks.colours = {
 blocks.blocks = {
     1: {
         cat: "control",
-        text: '<meta name="%s" content="%s">'
+        text: '<meta name="%s" content="%s">',
+        type: "block"
     },
     2:{
         cat: "control",
-        text: '<title></title>'
+        text: '<title></title>',
+        type: "block"
     },
     3:{
         cat: "other",
-        text: '<test></test>'
+        text: '<test></test>',
+        type: "block"
+    },
+    4:{
+        cat: "other",
+        text: "This is a c block",
+        type: "c"
     }
 };
 
 
-blocks.scripts = [
-    {
+blocks.scripts = {
+    1: {
         data:{
             position: {
                 x: 100,
                 y: 100
             }
-        },
-        content: [
-            {
-                data: {
-                    type: 'block', //Block data type
-                    id: 1 //How ever we want to identify blocks
-                },
-                inputs: {
-                    1: "test" //Can be a block object or string or what ever data type the input is.
-                }
-            },
-            {
-                data: {
-                    type: 'block', //Block data type
-                    id: 2 //How ever we want to identify blocks
-                },
-                inputs: {
-                    1: "test" //Can be a block object or string or what ever data type the input is.
-                },
-                content: [
-                    {
-                        data: {
-                            type: 'block', //Block data type
-                            id: 3 //How ever we want to identify blocks
-                        },
-                        inputs: {
-                            1: "test" //Can be a block object or string or what ever data type the input is.
-                        }
-                    }, {
-                        data: {
-                            type: 'block', //Block data type
-                            id: 2 //How ever we want to identify blocks
-                        },
-                        inputs: {
-                            1: "test" //Can be a block object or string or what ever data type the input is.
-                        }
-                    },
-                ]
-            },
-            {
-                data: {
-                    type: 'block', //Block data type
-                    id: 1 //How ever we want to identify blocks
-                },
-                inputs: {
-                    1: "test" //Can be a block object or string or what ever data type the input is.
-                }
-            },
-            {
-                data: {
-                    type: 'block', //Block data type
-                    id: 2 //How ever we want to identify blocks
-                },
-                inputs: {
-                    1: "test" //Can be a block object or string or what ever data type the input is.
-                },
-                content: [
-                    {
-                        data: {
-                            type: 'block', //Block data type
-                            id: 2 //How ever we want to identify blocks
-                        },
-                        inputs: {
-                            1: "test" //Can be a block object or string or what ever data type the input is.
-                        }
-                    }, {
-                        data: {
-                            type: 'block', //Block data type
-                            id: 2 //How ever we want to identify blocks
-                        },
-                        inputs: {
-                            1: "test" //Can be a block object or string or what ever data type the input is.
-                        },
-                        content: [
-                            {
-                                data: {
-                                    type: 'block', //Block data type
-                                    id: 2 //How ever we want to identify blocks
-                                },
-                                inputs: {
-                                    1: "test" //Can be a block object or string or what ever data type the input is.
-                                },
-                                content: [
-                                    {
-                                        data: {
-                                            type: 'block', //Block data type
-                                            id: 2 //How ever we want to identify blocks
-                                        },
-                                        inputs: {
-                                            1: "test" //Can be a block object or string or what ever data type the input is.
-                                        }
-                                    }, {
-                                        data: {
-                                            type: 'block', //Block data type
-                                            id: 2 //How ever we want to identify blocks
-                                        },
-                                        inputs: {
-                                            1: "test" //Can be a block object or string or what ever data type the input is.
-                                        }
-                                    },
-                                ]
-                            },
-                        ]
-                    },
-                ]
-            },
-        ]
+        }
     }
-];
+};
+
+blocks.placedBlocks = {
+    1: {
+        scriptId: 1,
+        parent:0, // 0 for script else a block ID for a c block
+        block: 1,
+        nth: 0
+    },
+    2: {
+        scriptId: 1,
+        parent:0, // 0 for script else a block ID for a c block
+        block: 1,
+        nth: 4
+    },
+    3: {
+        scriptId: 1,
+        parent:2, // 0 for script else a block ID for a c block
+        block: 1,
+        nth: 0
+    },
+};
+
 
 blocks.init = function() {
     document.addEventListener('mousedown',this.mouseDown);
@@ -210,34 +134,47 @@ blocks.drawNormalBlock = function(x,y,blockId){
     this.ctx.fillText(block.text,x+5,y+22);
 };
 
-blocks.getHeightC = function(content){
+blocks.getHeight = function(blockId) {
     var height = 0;
-    if (content != undefined) {
-        content.forEach(function (obj) {
-            if ('content' in obj) {
-                height += blocks.defaultHeight;
-                height += blocks.getHeightC(obj.content);
-                height += blocks.defaultHeight;
-            } else {
-                height += blocks.defaultHeight;
-            }
-        });
+    if (this.isType(blockId,'c')){
+        var done = false;
+        while (!done) {
+
+        }
     }
     return height;
 };
 
-blocks.renderCblock = function(x,y,blockId,content,child){
-    if (child == undefined) {
-        child = false;
+blocks.amountOfChildren = function(blockId) {
+    var count = 0;
+    for (var blockId in blocks.placedBlocks) {
+        if (blocks.placedBlocks.hasOwnProperty(blockId)) {
+            var block = blocks.placedBlocks[blockId];
+            if (block.parent == blockId) {
+                count++;
+            }
+        }
     }
+    return count;
+};
 
+blocks.isType = function(blockId,type){
+    var block = blocks.placedBlocks[blockId];
+    return blocks.blocks[block.block].type == type;
+};
+
+blocks.renderCblock = function(x,y,blockId,content,innerHeight){
     this.ctx = this.canvas.ctx;
     var block = this.blocks[blockId];
 
     var width = (block.text.length+1) * 11;
 
-    var height = this.defaultHeight;
-    var contentHeight = this.getHeightC(content);
+    var topBarHeight = this.defaultHeight;
+    var bottomBarHeight = this.defaultHeight;
+    
+    if (innerHeight < 1) {
+        innerHeight = this.defaultHeight;
+    }
     x += ui.rightPanel.width;
 
     var path=new Path2D();
@@ -253,51 +190,47 @@ blocks.renderCblock = function(x,y,blockId,content,child){
     path.lineTo(x+width,y);
 
     path.lineTo(x+width+2,y+2);
-    path.lineTo(x+width+2,y+(height -2));
-    path.lineTo(x+width,y+height);
+    path.lineTo(x+width+2,y+(topBarHeight -2));
+    path.lineTo(x+width,y+topBarHeight);
 
 
     /* Bottom puzzle of top bar */
-    path.lineTo(x+this.style.puzzle.width+42,y+height);
-    path.lineTo(x+this.style.puzzle.width+41,y+height+5);
-    path.lineTo(x+41,y+height+5);
-    path.lineTo(x+40,y+height);
+    path.lineTo(x+this.style.puzzle.width+42,y+topBarHeight);
+    path.lineTo(x+this.style.puzzle.width+41,y+topBarHeight+5);
+    path.lineTo(x+41,y+topBarHeight+5);
+    path.lineTo(x+40,y+topBarHeight);
 
-    path.lineTo(x+22,y+height);
-    path.lineTo(x+20,y+height + 2);
-    path.lineTo(x+20,y+height+contentHeight-2);
+    path.lineTo(x+22,y+topBarHeight);
+    path.lineTo(x+20,y+topBarHeight + 2);
+    path.lineTo(x+20,y+topBarHeight+innerHeight-2);
 
-    path.lineTo(x+22,y+height+contentHeight);
+    path.lineTo(x+22,y+topBarHeight+innerHeight);
 
     /* Top puzzle of bottom bar */
-    path.lineTo(x+40,y+height+contentHeight);
-    path.lineTo(x+41,y+height+contentHeight+5);
-    path.lineTo(x+this.style.puzzle.width+41,y+height+contentHeight+5);
-    path.lineTo(x+this.style.puzzle.width+42,y+height+contentHeight);
+    path.lineTo(x+40,y+topBarHeight+innerHeight);
+    path.lineTo(x+41,y+topBarHeight+innerHeight+5);
+    path.lineTo(x+this.style.puzzle.width+41,y+topBarHeight+innerHeight+5);
+    path.lineTo(x+this.style.puzzle.width+42,y+topBarHeight+innerHeight);
 
-    path.lineTo(x+width,y+height+contentHeight);
+    path.lineTo(x+width,y+topBarHeight+innerHeight);
 
-    path.lineTo(x+width+2,y+height+contentHeight+2);
-    path.lineTo(x+width+2,y+height+contentHeight+height -2);
-    path.lineTo(x+width,y+height+contentHeight+height);
+    path.lineTo(x+width+2,y+topBarHeight+innerHeight+2);
+    path.lineTo(x+width+2,y+topBarHeight+innerHeight+topBarHeight -2);
+    path.lineTo(x+width,y+topBarHeight+innerHeight+topBarHeight);
 
     /* Bottom puzzle of bottom bar */
-    path.lineTo(x+this.style.puzzle.width+22,y+height+contentHeight+height);
-    path.lineTo(x+this.style.puzzle.width+21,y+height+contentHeight+height+5);
-    path.lineTo(x+21,y+height+contentHeight+height+5);
-    path.lineTo(x+20,y+height+contentHeight+height);
+    path.lineTo(x+this.style.puzzle.width+22,y+topBarHeight+innerHeight+topBarHeight);
+    path.lineTo(x+this.style.puzzle.width+21,y+topBarHeight+innerHeight+topBarHeight+5);
+    path.lineTo(x+21,y+topBarHeight+innerHeight+topBarHeight+5);
+    path.lineTo(x+20,y+topBarHeight+innerHeight+topBarHeight);
 
-    path.lineTo(x+2,y+height+contentHeight+height);
-    path.lineTo(x,y+height+contentHeight+height - 2);
+    path.lineTo(x+2,y+topBarHeight+innerHeight+topBarHeight);
+    path.lineTo(x,y+topBarHeight+innerHeight+topBarHeight - 2);
 
     path.lineTo(x,y+2);
     path.lineTo(x+2,y);
 
-    if (!child) {
-        this.ctx.fillStyle = '#'+this.colours[this.blocks[blockId].cat];
-    }else{
-        this.ctx.fillStyle = '#'+addHexColor(this.colours[this.blocks[blockId].cat], '222222');
-    }
+    this.ctx.fillStyle = '#'+this.colours[this.blocks[blockId].cat];
 
     this.ctx.fill(path);
 
@@ -306,230 +239,49 @@ blocks.renderCblock = function(x,y,blockId,content,child){
     this.ctx.font = "20px Consolas";
     this.ctx.fillStyle = 'black';
     this.ctx.fillText(block.text,x+5,y+22);
-    var hIndex = blocks.defaultHeight;
-    content.forEach(function(obj){
-        if ('content' in obj) {
-            blocks.renderCblock(x - ui.rightPanel.width + 20, y + hIndex, obj.data.id, obj.content,!child);
-            hIndex += blocks.defaultHeight;
-            hIndex+=blocks.getHeightC(obj.content);
-            hIndex += blocks.defaultHeight;
-        }else{
-            blocks.drawBlock(x - ui.rightPanel.width + 20, y + hIndex, obj.data.id);
-            hIndex += blocks.defaultHeight;
-        }
-    });
+
 };
 
 blocks.render = function() {
-    ui.drawEditor();
-    blocks.scripts.forEach(function(script){
-        var index = 0;
-        var x = script.data.position.x + ui.rightPanel.width - 20;
-        var y = script.data.position.y - blocks.defaultHeight;
+    for (var key in blocks.scripts) {
+        if (blocks.scripts.hasOwnProperty(key)) {
+            var script = blocks.scripts[key],
+                content = this.getBlockIn(0, key),
+                x = script.position.x,
+                y = script.position.y;
 
-        var hIndex = blocks.defaultHeight;
-        var content = script.content;
+            var done = false;
 
-        content.forEach(function(obj){
-            if ('content' in obj) {
-                blocks.renderCblock(x - ui.rightPanel.width + 20, y + hIndex, obj.data.id, obj.content);
-                hIndex += blocks.defaultHeight;
-                hIndex+=blocks.getHeightC(obj.content);
-                hIndex += blocks.defaultHeight;
-            }else{
-                blocks.drawBlock(x - ui.rightPanel.width + 20, y + hIndex, obj.data.id);
-                hIndex += blocks.defaultHeight;
+            while (done==false) {
+
             }
-        });
-    });
+
+        }
+    }
+};
+
+blocks.getBlockIn = function(parentId,scriptId) {
+    var placedBlocks = [];
+    for (var blockId in blocks.placedBlocks) {
+        if (blocks.placedBlocks.hasOwnProperty(blockId)) {
+            var block = blocks.placedBlocks[blockId];
+            if (block.scriptId == scriptId && block.parent == parentId) {
+                placedBlocks.push(block);
+            }
+        }
+    }
+    return placedBlocks;
 };
 
 blocks.mouseDown = function(event) {
-    if (blocks.moving === false) {
-        var mouseX = event.clientX - ui.rightPanel.width;
-        var mouseY = event.clientY;
 
-        blocks.render();
-        blocks.scripts.forEach(function (object,scriptId) {
-            var index = 0;
-            var x = object.data.position.x;
-            var y = object.data.position.y;
-
-            var movingBlocks = [];
-            var new_content = [];
-            var addBlocks = false;
-            blocks.callStack = 0;
-            blocks.scripts[scriptId].content = blocks.onClickChild(mouseX, mouseY, x, y, object.content);
-            console.log(blocks.callStack);
-
-        });
-    }
-};
-
-blocks.onClickChild = function(mouseX, mouseY, blockX, blockY, content) {
-    blocks.callStack++;
-    var index = 0;
-    var nextBlocks = [];
-    var addBlocks = false;
-    var newContent = [];
-    content.forEach(function(obj,key){
-        var y = blockY + index;
-        var x = blockX;
-        if (addBlocks === false) {
-            if ('content' in obj) {
-                obj.content = blocks.onClickChild(mouseX, mouseY, x + 20, y + blocks.defaultHeight, obj.content);
-                index += blocks.getHeightC(obj.content);
-                index += blocks.defaultHeight;
-            }
-        }
-
-        var height = blocks.defaultHeight;
-        var width = (blocks.blocks[obj.data.id].text.length + 1) * 11;
-
-        if (mouseX > x && mouseX < x + width) {
-            if (mouseY > y && mouseY < y + height) {
-                addBlocks = true;
-                blocks.dragOffset.x = x-mouseX;
-            }
-        }
-
-        if(addBlocks){
-            nextBlocks.push(obj);
-        }else{
-            newContent.push(obj);
-        }
-
-        index+= blocks.defaultHeight;
-
-    });
-
-    /* Create new script with blocks under clicked block */
-    if (addBlocks){
-        var newscript = {
-            data: {
-                position: {
-                    x: mouseX,
-                    y: mouseY
-                }
-            },
-            content: []
-        };
-        newscript.content = nextBlocks;
-
-        blocks.scripts.push(newscript);
-        blocks.render();
-        blocks.moving = true;
-    }
-    return newContent;
 };
 
 blocks.mouseMove = function(event) {
-    var mouseX = event.clientX - ui.rightPanel.width + blocks.dragOffset.x;
-    var mouseY = event.clientY - (blocks.defaultHeight / 2);
-    if (blocks.moving === true) {
 
-        /* Set min mouse posstions */
-        if (mouseX < 1){
-            mouseX = 1;
-        }
-
-        if (mouseY < 1){
-            mouseY = 1;
-        }
-
-
-        blocks.scripts[blocks.scripts.length - 1].data.position.x = mouseX;
-        blocks.scripts[blocks.scripts.length - 1].data.position.y = mouseY;
-        blocks.render();
-    }
 };
 
 blocks.mouseUp = function(event){
-    if (blocks.moving) {
-        var mouseX = event.clientX - ui.rightPanel.width + blocks.dragOffset.x;
-        var mouseY = event.clientY - (blocks.defaultHeight / 2);
-        blocks.moving = false;
 
-
-        blocks.scripts.forEach(function (object,scriptId) {
-            var index = 0;
-            var x = object.data.position.x;
-            var y = object.data.position.y;
-
-            var newContent = blocks.findSnap(mouseX, mouseY, x, y, object.content);
-
-            blocks.scripts[scriptId].content = newContent;
-
-
-        });
-
-        blocks.render();
-    }
 };
 
-blocks.findSnap = function(mouseX, mouseY, blockX, blockY, content) {
-    var index = 0;
-    var nextBlocks = [];
-    var addBlocks = false;
-    var newContent = [];
-    content.forEach(function(obj,key){
-        var y = blockY + index;
-        var x = blockX;
-        var cBlockHeight = 0;
-        if (addBlocks === false) {
-            if ('content' in obj) {
-                obj.content = blocks.findSnap(mouseX, mouseY, x + 20, y + blocks.defaultHeight, obj.content);
-                cBlockHeight += blocks.getHeightC(obj.content);
-            }
-        }
-
-        var height = blocks.defaultHeight;
-        var width = (blocks.blocks[obj.data.id].text.length + 1) * 11;
-
-        if ('content' in obj){
-            /* If snapping to top of c block */
-            if (mouseX > x && mouseX < x + width) {
-                if (mouseY > y && mouseY < y + height) {
-                    var toSnap = blocks.scripts[blocks.scripts.length - 1].content;
-                    toSnap.forEach(function (object,key) {
-                        obj.content.splice(key,0,object);
-                    });
-                    delete blocks.scripts[blocks.scripts.length - 1];
-                }
-            }
-            newContent.push(obj);
-
-            /* If snapping to bottom of c block */
-            if (mouseX > x && mouseX < x + width) {
-                if (mouseY > y + cBlockHeight+ height && mouseY < y + height + cBlockHeight+ height) {
-                    var toSnap = blocks.scripts[blocks.scripts.length - 1].content;
-                    toSnap.forEach(function (object,key) {
-                        newContent.push(object);
-                    });
-                    delete blocks.scripts[blocks.scripts.length - 1];
-                }
-            }
-        }else {
-            newContent.push(obj);
-
-            if (mouseX > x && mouseX < x + width) {
-                if (mouseY > y && mouseY < y + height) {
-                    var toSnap = blocks.scripts[blocks.scripts.length - 1].content;
-                    toSnap.forEach(function (object) {
-                        newContent.push(object);
-                    });
-                    delete blocks.scripts[blocks.scripts.length - 1];
-                }
-            }
-        }
-        if (cBlockHeight > 0){
-            index+= cBlockHeight;
-            index+= blocks.defaultHeight;
-
-        }
-        index+= blocks.defaultHeight;
-
-    });
-
-    return newContent;
-};
